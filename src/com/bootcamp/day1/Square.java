d7578682203f54cfae5f3d92bb080abc52ae4fa3
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-03-18 22:22:13 +0530

    implement square class
    implement square class inherititing from rectangle

--- src/com/bootcamp/day1/Square.java
+++ src/com/bootcamp/day1/Square.java
@@ -1,7 +1,8 @@
 package com.bootcamp.day1;
 
 public class Square extends Rectangle{
-    public Square(Dimension length, Dimension breadth) {
-        super(length, breadth);
+
+    public Square(Dimension side) {
+        super(side, side);
     }
 }
--- test/com/bootcamp/day1/SquareTest.java
+++ test/com/bootcamp/day1/SquareTest.java
@@ -8,16 +8,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class SquareTest {
     Square sqr;
     @BeforeEach
-    void setUp() {
-        sqr = new Square(2);
+    void setUp() throws NegativeDimensionException {
+        sqr = new Square(new Dimension(2));
     }
 
     @Test
     void calculateArea() {
-
+        double areaOfSquare = sqr.calculateArea();
+        assertEquals(4,areaOfSquare,1);
     }
 
     @Test
     void calculatePerimeter() {
+        double perimeterOfSquare = sqr.calculatePerimeter();
+        assertEquals(8,perimeterOfSquare,1);
     }
 }
