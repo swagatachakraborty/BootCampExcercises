d3fdaa8efc8e43746ed1f30cd74d1d31366b6c18
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-03-19 13:00:24 +0530

    Implement OR method for Probability class.

--- src/com/bootcamp/day2/Probability.java
+++ src/com/bootcamp/day2/Probability.java
@@ -1,6 +1,8 @@
 package com.bootcamp.day2;
 
 class Probability {
+    private static final int LOWER_BOUND = 0;
+    private static final int UPPER_BOUND = 1;
     private double value;
 
     Probability(double value) throws InvalidRangeException {
@@ -11,11 +13,22 @@ class Probability {
     }
 
     private static boolean isValid(double value) {
-        return value >= 0 && value <= 1;
+        return value >= LOWER_BOUND && value <= UPPER_BOUND;
     }
 
-    Probability complement() {
-        return new Probability(1 - this.value);
+    Probability not() {
+        return new Probability(UPPER_BOUND - this.value);
+    }
+
+    Probability and(Probability other) {
+        return new Probability(this.value * other.value);
+    }
+
+    Probability or(Probability other) {
+        Probability complementOfThis = this.not();
+        Probability complementOfOther = other.not();
+        Probability noOccurrence = complementOfThis.and(complementOfOther);
+        return noOccurrence.not();
     }
 
     @Override
@@ -23,7 +36,6 @@ class Probability {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Probability that = (Probability) o;
-        return Double.compare(that.value, value) == 0;
+        return Double.compare(that.value, this.value) == 0;
     }
 }
-
--- test/com/bootcamp/day2/ProbabilityTest.java
+++ test/com/bootcamp/day2/ProbabilityTest.java
@@ -5,20 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 class ProbabilityTest {
 
-    private Probability expected;
-
     @Test
-    @DisplayName("should throw InvalidRangeException when the provided probability is less than 0.")
+    @DisplayName("should throw InvalidRangeException when the provided probability is less than 0")
     void invalidRangeExceptionForLessThanZero() {
         InvalidRangeException invalidRangeException = assertThrows(InvalidRangeException.class, () -> new Probability(-1));
         assertEquals("Invalid Range", invalidRangeException.getMessage());
     }
 
     @Test
-    @DisplayName("should throw InvalidRangeException when the provided probability is greater than 1.")
+    @DisplayName("should throw InvalidRangeException when the provided probability is greater than 1")
     void invalidRangeExceptionForGreaterThanOne() {
         InvalidRangeException invalidRangeException = assertThrows(InvalidRangeException.class, () -> new Probability(1.1));
         assertEquals("Invalid Range", invalidRangeException.getMessage());
@@ -31,11 +28,29 @@ class ProbabilityTest {
     }
 
     @Test
-    @DisplayName("should give the complement of the given probability")
+    @DisplayName("should give the not of the given probability")
     void complement() {
         Probability probability = new Probability(0.5);
+        Probability expected = new Probability(0.5);
+        assertEquals(expected, probability.not());
+    }
 
-        expected = new Probability(0.5);
-        assertEquals(expected, probability.complement());
+    @Test
+    @DisplayName("Should return the probability of co-occurrence of two tails while flipping two coins")
+    void coOccurrenceOfTwoTails() {
+        Probability probabilityOfCoin1 = new Probability(0.5);
+        Probability probabilityOfCoin2 = new Probability(0.5);
+        Probability expected = new Probability(0.25);
+        assertEquals(expected, probabilityOfCoin1.and(probabilityOfCoin2));
+
+    }
+
+    @Test
+    @DisplayName("Should return the probability of at least single occurrence of tail while flipping two coins")
+    void atLeastSingleOccurrenceOfTails() {
+        Probability probabilityOfCoin1 = new Probability(0.5);
+        Probability probabilityOfCoin2 = new Probability(0.5);
+        Probability expected = new Probability(0.75);
+        assertEquals(expected, probabilityOfCoin1.or(probabilityOfCoin2));
     }
 }
